7d649d6f3cad68db472c3e7a54fb87b8b871db64
Yusuf Kilmamas <72572140+yusufk187@users.noreply.github.com>
2024-04-16 11:48:15 +0200

    Add files via upload

---
+++ onesiteaday/dayone/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["/Users/yusufkilmamas/Desktop/bit-academy/onesiteaday/dayone/dayone.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
